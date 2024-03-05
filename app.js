7eec48c15edcc77cf4697efe1c717533875341b8
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2024-03-05 10:11:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ let bodyParser = require('body-parser');
 let multer = require('multer');
 let upload = multer();
 
-let express = require('express')
-let app = express()
+let express = require('express');
+let app = express();
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('cadastro')
-})
+    res.render('cadastro');
+});
 
+app.get('/pos-cadastro', (req, res) => {
+    res.render('Tela_poscadastro');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Servidor rodando na porta: ' + PORT));
